fb7bf6005c084ce4c2e07ee8d1b0f7dbf5797f61
unknown <sivasuryaabd>
2026-07-29 21:23:42 +0530

    Add some change.

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -151,7 +151,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
 #     "http://127.0.0.1:3000",
