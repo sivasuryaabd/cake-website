634b25b985a43041aa3cdf6f00fa92bdba2e8fe3
unknown <sivasuryaabd>
2026-08-01 16:07:06 +0530

    change static files...

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -192,5 +192,14 @@ CLOUDINARY_STORAGE={
 }
 
 DEFAULT_FILE_STORAGE= 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STORAGES={
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
