e1c6bc69d6775047893d6c14edb59a1a8de0f51b
unknown <sivasuryaabd>
2026-08-01 13:23:17 +0530

    add superuser

--- backend/project/build.sh
+++ backend/project/build.sh
@@ -3,3 +3,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata products_fixture.json
+
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'tiger')" | python manage.py shell
